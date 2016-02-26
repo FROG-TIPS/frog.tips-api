42a7fc8bee75eaf19218cfb129fd83340c4b870f
tyrands <t@rands.unfunny>
2016-02-26 01:53:03 -0500

    GARY

--- src/frog/plop.py
+++ src/frog/plop.py
@@ -4,7 +4,7 @@ from flask import Blueprint, render_template
 from frog.christmas_tree_monster import some_tips_please_sir
 
 
-endpoint = Blueprint('gary', __name__, url_prefix='/~GARY', template_folder='gary/templates', static_folder='gary/static', static_url_path='/static/gary')
+endpoint = Blueprint('gary', __name__, url_prefix='/~GARY')
 
 @endpoint.route('/DE/MANUAL_2//DAS_IST_MEIN_FROG.ASPX')
 def frog_manual():
@@ -26,4 +26,4 @@ def frog_manual():
             tip = re.sub(find, replace, tip)
         tips.append(tip)
 
-    return render_template('das_ist_mein_frog.html', tips=tips)
+    return render_template('gary/das_ist_mein_frog.html', tips=tips)
