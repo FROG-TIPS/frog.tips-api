583062e148a92d25ae97155c2f36caa65ff17245
Perl Is Weird <perlisweird@gmail.com>
2016-04-30 19:48:09 -0700

    TAKE IT TO THE LIMIT

--- src/frog/christmas_tree_monster.py
+++ src/frog/christmas_tree_monster.py
@@ -76,10 +76,10 @@ def just_a_tip_oof_right_there(num):
         return result._asdict()
 
 
-def the_search_for_spock(fat_blob):
-    fat_blob = fat_blob.replace(' ', '%')
+def the_search_for_spock(fat_blob, limit=200):
+    fat_blob = fat_blob.replace(' ', '%').upper()
     results = db.session.query(Tip.number, Tip.tip) \
                        .filter(Tip.tip.like('%{0}%'.format(fat_blob), escape='\\')) \
-                       .limit(10) \
+                       .limit(limit) \
                        .all()
     return [result._asdict() for result in results]
