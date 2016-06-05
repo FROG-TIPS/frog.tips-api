e6c299fa95757ed6e99850126005ce1159f50791
Perl Is Weird <perlisweird@gmail.com>
2016-06-05 16:35:00 -0700

    UHHHH

--- src/frog/boners.py
+++ src/frog/boners.py
@@ -8,4 +8,8 @@ app = Blueprint('app', __name__, url_prefix='/')
 @app.route('')
 def root():
     tips = some_tips_please_sir()
-    return render_template('index.html', preloaded_tips={'tips': tips})
+
+    # DISPLAY THIS TIP IN THE META DESCRIPTION FOR ALL THE CHAT PROGRAMS KIDS ARE USING THESE DAYS
+    titular_tip = tips[0]['tip'] if tips else None
+
+    return render_template('index.html', preloaded_tips={'tips': tips}, titular_tip=titular_tip)
