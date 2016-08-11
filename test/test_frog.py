f22edda98e049d8ca17fa5cf9a775801e9526581
Perl Is Weird <perlisweird@gmail.com>
2016-08-11 15:21:53 -0700

    UGH

--- test/test_frog.py
+++ test/test_frog.py
@@ -2,7 +2,7 @@ import os
 
 MASTER_AUTH_PHRASE = 'CROAKING'
 
-os.environ['FLASK_CONFIG'] = os.path.join(os.path.dirname(__file__), '../config/test.py')
+os.environ['FLASK_CONFIG'] = os.path.join(os.path.dirname(__file__), '../src/frog/config/test.py')
 os.environ['FLASK_MASTER_AUTH_PHRASE'] = MASTER_AUTH_PHRASE
 
 
